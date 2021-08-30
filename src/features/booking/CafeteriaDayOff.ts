f93c4109aa60a0d5b1fabc17cb622570e5093574
potados99 <potados99@gmail.com>
2021-08-30 20:54:32 +0900

    chore: 누락된 @TransformDate() 추가

--- src/features/booking/CafeteriaDayOff.ts
+++ src/features/booking/CafeteriaDayOff.ts
@@ -2,6 +2,7 @@ import BaseEntity from '@/core/entity/BaseEntity';
 import Entity from '@/core/entity/decorator/Entity';
 import Field from '@/core/entity/decorator/Field';
 import {numberOverZero} from '@/core/component/common/validation';
+import TransformDate from '@/core/entity/decorator/TransformDate';
 
 @Entity({displayName: '휴업 일정'})
 export default class CafeteriaDayOff extends BaseEntity {
@@ -12,8 +13,10 @@ export default class CafeteriaDayOff extends BaseEntity {
   cafeteriaId: number;
 
   @Field({displayName: '휴업 시작 시간'})
+  @TransformDate()
   startsAt: Date;
 
   @Field({displayName: '휴업 종료 시간'})
+  @TransformDate()
   endsAt: Date;
 }
