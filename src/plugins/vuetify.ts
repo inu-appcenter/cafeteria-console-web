9fcacec6236904aae1b31b9de9b3148784cb30c1
potados99 <potados99@gmail.com>
2021-09-26 02:12:24 +0900

    chore: Vuetify dialog 가져옴

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,10 +1,13 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 import VuetifyConfirm from 'vuetify-confirm';
+import VuetifyDialog from 'vuetify-dialog';
+import 'vuetify-dialog/dist/vuetify-dialog.css';
 
 const vuetify = new Vuetify({});
 
 Vue.use(Vuetify); // This line necessary
 Vue.use(VuetifyConfirm, {vuetify});
+Vue.use(VuetifyDialog, {context: {vuetify}});
 
 export default vuetify;
