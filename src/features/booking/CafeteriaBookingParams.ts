9cad49391e5c60781d116b33602f9b9f480e89b2
potados99 <potados99@gmail.com>
2021-10-08 01:00:17 +0900

    refactor: CafeteriaBookingParams 변경

--- src/features/booking/CafeteriaBookingParams.ts
+++ src/features/booking/CafeteriaBookingParams.ts
@@ -1,7 +1,7 @@
-import BaseEntity from '@/core/entity/BaseEntity';
-import Entity from '@/core/entity/decorator/Entity';
 import Field from '@/core/entity/decorator/Field';
-import {numberOverZero, timeRangeString} from '@/core/component/common/validation';
+import Entity from '@/core/entity/decorator/Entity';
+import BaseEntity from '@/core/entity/BaseEntity';
+import {numberOverZero} from '@/core/component/common/validation';
 
 @Entity({displayName: '예약 옵션'})
 export default class CafeteriaBookingParams extends BaseEntity {
@@ -11,18 +11,6 @@ export default class CafeteriaBookingParams extends BaseEntity {
   @Field({displayName: '연관된 식당의 식별자', validate: numberOverZero})
   cafeteriaId: number;
 
-  @Field({displayName: '수용 가능 인원', validate: numberOverZero})
-  capacity: number;
-
-  @Field({displayName: '예약을 받는 시간대', validate: timeRangeString})
-  acceptTimeRange: string;
-
-  @Field({displayName: '예약 시간대의 간격(분)', validate: numberOverZero})
-  intervalMinutes: number;
-
   @Field({displayName: '입장 후 식당에 머무르는 기간(분)', validate: numberOverZero})
-  durationMinutes: number;
-
-  @Field({displayName: '입장 시간 전후 허용 오차(분)', validate: numberOverZero})
-  toleranceMinutes: number;
+  userStaysForMinutes: number;
 }
