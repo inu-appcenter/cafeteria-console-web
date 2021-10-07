ead6b3816b35f34cab95569cb1fbd197be64b17c
potados99 <potados99@gmail.com>
2021-10-08 01:00:51 +0900

    chore: 누락된 설명 추가

--- src/features/cafeteria/Corner.ts
+++ src/features/cafeteria/Corner.ts
@@ -1,6 +1,6 @@
-import BaseEntity from '@/core/entity/BaseEntity';
-import Entity from '@/core/entity/decorator/Entity';
 import Field from '@/core/entity/decorator/Field';
+import Entity from '@/core/entity/decorator/Entity';
+import BaseEntity from '@/core/entity/BaseEntity';
 import {numberNonNegative, numberOverZero, stringNotEmpty} from '@/core/component/common/validation';
 
 @Entity({displayName: '코너'})
@@ -8,15 +8,15 @@ export default class Corner extends BaseEntity {
   @Field({displayName: '식별자', validate: numberOverZero})
   id: number;
 
-  @Field({validate: stringNotEmpty})
+  @Field({displayName: '코너 이름', validate: stringNotEmpty})
   name: string;
 
-  @Field({validate: stringNotEmpty})
+  @Field({displayName: '코너 표시 이름', validate: stringNotEmpty})
   displayName: string;
 
-  @Field({validate: numberNonNegative})
+  @Field({displayName: '이용 가능 시간(4|2|1)', validate: numberNonNegative})
   availableAt: number;
 
-  @Field({validate: numberOverZero})
+  @Field({displayName: '속한 식당의 식별자', validate: numberOverZero})
   cafeteriaId: number;
 }
