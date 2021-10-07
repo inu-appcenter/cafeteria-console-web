a397011806928df87d6b5f59baa20803a9aaa919
potados99 <potados99@gmail.com>
2021-10-08 01:32:18 +0900

    chore: 식당 예약 파라미터는 식별자 표시

--- src/features/booking/CafeteriaBookingParams.ts
+++ src/features/booking/CafeteriaBookingParams.ts
@@ -5,7 +5,7 @@ import {numberOverZero} from '@/core/component/common/validation';
 
 @Entity({displayName: '예약 옵션'})
 export default class CafeteriaBookingParams extends BaseEntity {
-  @Field({displayName: '식별자', visible: false, mutable: false})
+  @Field({displayName: '식별자', visible: true, mutable: false})
   id: number;
 
   @Field({displayName: '연관된 식당의 식별자', validate: numberOverZero})
