2cadf197f126ebe46e775c6ecfa12f5884811291
potados99 <potados99@gmail.com>
2022-01-24 17:19:06 +0900

    chore: copyright 추가

--- src/core/entity/Editable.ts
+++ src/core/entity/Editable.ts
@@ -1,4 +1,23 @@
 /**
+ * This file is part of INU Cafeteria.
+ *
+ * Copyright 2022 INU Global App Center <potados99@gmail.com>
+ *
+ * INU Cafeteria is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * INU Cafeteria is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
  * Vue 컴포넌트에서 사용할 수 있는,
  * 편집 상태를 가지는 클래스를 정의할 때에 씁니다.
  */
