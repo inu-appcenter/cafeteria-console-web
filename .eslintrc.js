eddb5beadc9a9c9f96a7fc4d52ec92cf07c8d713
potados99 <potados99@gmail.com>
2022-02-28 05:01:27 +0900

    chore: 워닝 ㅃ2

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,8 +33,8 @@ module.exports = {
     ecmaVersion: 2020,
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-console': 'off',
+    'no-debugger': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-inferrable-types': 'off',
     'vue/no-mutating-props': 'off',
